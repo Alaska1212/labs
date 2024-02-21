d8cc50f10d85ac74d0bfd642404dfa7e31b17a49
Polina Puchek <polinapuchek89@gmail.com>
2024-02-21 21:26:42 +0200

    rec2.0

--- laba1/Rectangle.py
+++ laba1/Rectangle.py
@@ -1,20 +1,15 @@
 #прямокутник
 class Rectangle:
-    def __init__(self, a: float, b: float, c: float, d: float):
-        adges = [a, b, c, d]
-        adges.sort()
-        a, b, c, d = adges
-        assert a == b and c == d
+    def __init__(self, a: float, b: float):
         self.a = a
         self.b = b
-        self.c = c
-        self.d = d
+
     def perimeter(self):
-        return self.a + self.b + self.c + self.d
+        return (self.a + self.b)*2
 
     def area(self):
-        return self.a * self.c
+        return self.a * self.b
 
 if __name__ == '__main__':
-    t = Rectangle(3, 4, 3, 4)
+    t = Rectangle(3, 4)
     print(t.area())
