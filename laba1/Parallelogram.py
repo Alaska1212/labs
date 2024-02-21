72971fea3fc158f192dc3cdcf5422a7f04f498c5
Polina Puchek <polinapuchek89@gmail.com>
2024-02-21 21:25:51 +0200

    par

--- laba1/Parallelogram.py
+++ laba1/Parallelogram.py
@@ -1,1 +1,18 @@
 #паралелограм
+class Parallelogram:
+    def __init__(self, a: float, b: float, h: float):
+        adges = [a, b, h]
+        adges.sort()
+        h, a, b = adges
+        self.a = a
+        self.b = b
+        self.h = h
+
+    def perimeter(self):
+        return (self.a + self.b)*2
+    def area(self):
+        return (self.h * self.b)
+
+if __name__ == '__main__':
+    t = Parallelogram(8, 6, 10)
+    print(t.area())
