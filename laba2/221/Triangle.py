5b71c05e6c9dfb872ac50088283781fcbcf54f3c
Polina Puchek <polinapuchek89@gmail.com>
2024-03-06 12:52:38 +0200

    end trangle2

--- laba2/221/Triangle.py
+++ laba2/221/Triangle.py
@@ -1,21 +1,20 @@
-#23.02
 import turtle
+from random import randint
 
 class Triangle:
-    default_color = "#FF0000"
-
+    default_color = None
     def __init__(self, x1, y1, x2, y2):
         self.vertex1 = (x1, y1)
         self.vertex2 = (x2, y2)
-        self.color = Triangle.default_color
+        self.color =   None
 
-        self.position = (0, 0)
+        self.position = (randint(-200, 200), randint(-200, 200))
 
-    def set_position(self, x , y):
-        self.position = (x , y)
+    def set_position(self, x, y):
+        x = randint(-100, 100)
+        y = randint(-100, 100)
+        self.position = (x, y)
 
-    def set_color(self, color):
-        self.color = color
 
     def calc_abs_pos(self):
         v1 = (self.position[0] + self.vertex1[0],
@@ -23,10 +22,10 @@ class Triangle:
         v2 = (self.position[0] + self.vertex2[0],
               self.position[1] + self.vertex2[1])
         return v1, v2
-
     def draw(self):
         v1, v2 = self.calc_abs_pos()
-        turtle.color(self.color)
+        random_color = "#{:06x}".format(randint(0, 0xFFFFFF))
+        turtle.color(random_color)
         turtle.up()
         turtle.setpos(*self.position)
         turtle.down()
@@ -34,20 +33,16 @@ class Triangle:
         turtle.goto(*v2)
         turtle.setpos(*self.position)
         turtle.up()
-        turtle.color(Triangle.default_color)
+
+
 
 if __name__ == '__main__':
     turtle.speed(0)
 
-    triangle = Triangle(100, 100, 100, 0)
-    triangle.draw()
-    turtle.clear()
-
-    triangle.set_color("#658569")
     for i in range(100):
-        #turtle.clear()
-        triangle.set_position(-2*i , -i)
+        triangle = Triangle((randint(-100, 100)), (randint(-100, 100)), (randint(-100, 100)), (randint(-100, 100)))
+        triangle.draw()
+        triangle.set_position(-2 * i, -i)
         triangle.draw()
 
-
-    turtle.mainloop()
+turtle.mainloop()
