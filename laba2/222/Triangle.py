faba59d143eb42cdf69f473c72099661aa992f9d
Polina Puchek <polinapuchek89@gmail.com>
2024-03-06 22:14:06 +0200

    rotation of the triangle

--- laba2/222/Triangle.py
+++ laba2/222/Triangle.py
@@ -1,5 +1,5 @@
 import turtle
-turtle.shape("turtle")
+
 from random import randint
 
 class Triangle:
@@ -16,6 +16,18 @@ class Triangle:
               self.position[1] + self.vertex2[1])
         return v1, v2
 
+    def rotate(self, angle):
+        new_vertex1 = (
+            self.vertex1[0] * cos(radians(angle)) - self.vertex1[1] * sin(radians(angle)),
+            self.vertex1[0] * sin(radians(angle)) + self.vertex1[1] * cos(radians(angle))
+        )
+        new_vertex2 = (
+            self.vertex2[0] * cos(radians(angle)) - self.vertex2[1] * sin(radians(angle)),
+            self.vertex2[0] * sin(radians(angle)) + self.vertex2[1] * cos(radians(angle))
+        )
+        self.vertex1 = new_vertex1
+        self.vertex2 = new_vertex2
+
     def draw(self):
         v1, v2 = self.calc_abs_pos()
         turtle.up()
@@ -27,11 +39,16 @@ class Triangle:
         turtle.up()
 
 if __name__ == '__main__':
-    turtle.speed(1)
+    from math import cos, sin, radians
+
+    turtle.speed(0)
+
     triangle = Triangle((randint(-100, 200)), (randint(-100, 200)), (randint(-100, 200)), (randint(-100, 200)))
 
 for _ in range(120):
     triangle.draw()
+    triangle.rotate(3)
     turtle.clear()
-    triangle.draw()
+
+
 turtle.mainloop()
