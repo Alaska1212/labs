26f8009e4c98c77b7406c8dbfb54feaf7cc3800e
Polina Puchek <polinapuchek89@gmail.com>
2024-05-15 19:56:44 +0300

    board end

--- project/CheckersBoard.py
+++ project/CheckersBoard.py
@@ -1,8 +1,10 @@
 import tkinter as tk
 
 class CheckersBoard(tk.Frame):
-    def __init__(self):
+    def __init__(self, color1="black", color2="white"):
         super().__init__()
+        self.color1 = color1
+        self.color2 = color2
         self.pack()
         self.board = tk.Canvas(self, width=400, height=400)
         self.board.pack()
@@ -11,9 +13,9 @@ class CheckersBoard(tk.Frame):
     def draw_board(self):
         for i in range(8):
             for j in range(8):
-                color = "black" if (i + j) % 2 == 0 else "white"
+                color = self.color1 if (i + j) % 2 == 0 else self.color2
                 self.board.create_rectangle(j * 50, i * 50, (j + 1) * 50, (i + 1) * 50, fill=color)
 
 root = tk.Tk()
-app = CheckersBoard()
+app = CheckersBoard(color1="peru", color2="grey6")
 app.mainloop()
