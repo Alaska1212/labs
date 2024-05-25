9b29ec4545706b6dd12cc777bbf3aea88a4df032
Polina Puchek <polinapuchek89@gmail.com>
2024-05-25 17:52:42 +0300

    111

--- laba1/part2/Vector.py
+++ laba1/part2/Vector.py
@@ -1,22 +1,46 @@
 class Vector:
-    def __init__(self, coordinates):
-        self.coordinates = coordinates
+    def __init__(self, *args):
+        if len(args) == 1 and isinstance(args[0], Vector):
+            vect = args[0]
+            self.coords = vect.coords
+        else:
+            self.coords = args
 
-    def __str__(self):
-        return str(self.coordinates)
+    def show(self):
+        return self.coords
 
-    def dimension(self): #розмірність
-        return len(self.coordinates)
+    def dim(self):
+        return len(self.coords)
 
-    def length(self): #довжина
-        return sum(x**2 for x in self.coordinates)**0.5
+    def module(self):
+        sum = 0
+        for i in self.coords:
+            sum += i**2
 
-    def average(self):
-        return sum(self.coordinates) / len(self.coordinates)
+        return sum ** 0.5
 
-    def max_component(self):
-        return max(self.coordinates)
+    def mean(self):
+        sum = 0
+        for i in self.coords:
+            sum += i
 
-    def min_component(self):
-        return min(self.coordinates)
+        mean = sum / len(self.coords)
+        return mean
 
+    def min(self):
+        min1 = min(self.coords)
+        return min1
+
+    def max(self):
+        max1 = max(self.coords)
+        return max1
+
+
+if __name__ == "__main__":
+    v = Vector(1, 2, 3)
+    v.show()
+    print(v.dim())
+    print(v.module())
+    print(v.mean())
+    print(v.min())
+    print(v.max())
