e3e6df94ee29274f14f5ac35f5b9e5e5a471189e
Polina Puchek <polinapuchek89@gmail.com>
2024-06-12 12:10:05 +0300

    end 02.f

---
+++ laba7/2/f.py
@@ -0,0 +1,19 @@
+def g(x, k):
+    x0 = x
+    yield x0
+    for i in range(1, k+1):
+        x0 *= x ** 2 / (2 * i * (2 * i + 1))
+        yield x0
+
+
+x = int(input('х = '))
+x0 = x
+k = int(input('член послідовності: '))
+for i in range(1, k+1):
+    x0 *= x ** 2 / (2 * i * (2 * i + 1))
+
+x0 = x
+for i in g(x, k):
+    x0 = i
+
+print(x0)
