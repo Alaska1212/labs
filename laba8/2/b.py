11933dab529c4b93b31099ca012fc86343d0b948
Polina Puchek <polinapuchek89@gmail.com>
2024-06-13 20:38:07 +0300

    end 02.b

--- laba8/2/b.py
+++ laba8/2/b.py
@@ -1,14 +1,14 @@
-from math import sinh
+from math import log, e
 
 x = float(input('х = '))
 x1 = x
 s = x
 eps = 10 ** -13
-n = 2
+n = 1
 while abs(x1) > eps:
-    x1 *= x ** 2 / ((2 * n - 2) * (2 * n - 1))
+    x1 *= -x * (n) / n
     n += 1
     s += x1
 
 print(s)
-print(sinh(x))
+print(log((1+x), e))
